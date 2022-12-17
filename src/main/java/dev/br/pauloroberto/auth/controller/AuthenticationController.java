a21f2fba8bdd2433a7a4aee26b363c0585af575a
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-17 12:11:45 -0300

    The authenticate method's constructor now has a TokenService attribute. Additionally, the method encapsulates the call of the generateToken method from the TokenService class. The authenticate method returns a ResponseEntity with the newly generated token's data.

--- src/main/java/dev/br/pauloroberto/auth/controller/AuthenticationController.java
+++ src/main/java/dev/br/pauloroberto/auth/controller/AuthenticationController.java
@@ -1,6 +1,9 @@
 package dev.br.pauloroberto.auth.controller;
 
 import dev.br.pauloroberto.auth.domain.user.AuthenticationDto;
+import dev.br.pauloroberto.auth.domain.user.User;
+import dev.br.pauloroberto.auth.security.TokenDto;
+import dev.br.pauloroberto.auth.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,17 +18,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class AuthenticationController {
     private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
-    public AuthenticationController(AuthenticationManager authenticationManager) {
+    public AuthenticationController(AuthenticationManager authenticationManager, TokenService tokenService) {
         this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
     }
 
     @PostMapping
     public ResponseEntity<?> authenticate(@RequestBody @Valid AuthenticationDto authData) {
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(authData.username(), authData.password());
-        Authentication authentication = authenticationManager.authenticate(token);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                authData.username(),
+                authData.password()
+        );
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
 
+        // This is the user that was authenticated
+        // getPrincipal() returns the user object that was authenticated
+        String token = tokenService.generateToken((User) authentication.getPrincipal());
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new TokenDto(token));
     }
 }
