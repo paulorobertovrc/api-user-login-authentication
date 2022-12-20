4c38d104bd48bab5e71f6d1d1f736463b3bd44e9
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-20 08:02:27 -0400

    Added roles column

--- src/main/resources/db/migration/V1__create-table-users.sql
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -2,6 +2,7 @@ create table users (
     id int not null auto_increment,
     username varchar(255) not null unique,
     password varchar(255) not null,
+    roles varchar(255) not null,
 
     primary key (id)
 );
