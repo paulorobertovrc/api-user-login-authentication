a493e05825c5c847aa8e844217905b2cb61aaa2d
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-21 08:56:09 -0400

    The constructor now instantiates a SimpleGrantedAuthority object with the defined role

--- src/main/java/dev/br/pauloroberto/auth/domain/user/AdminUser.java
+++ src/main/java/dev/br/pauloroberto/auth/domain/user/AdminUser.java
@@ -3,6 +3,7 @@ package dev.br.pauloroberto.auth.domain.user;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 
 @Entity
@@ -13,5 +14,6 @@ public class AdminUser extends User {
 
     public AdminUser(Long id, String username, String password) {
         super(id, username, password, "ADMIN");
+        new SimpleGrantedAuthority("ROLE_ADMIN");
     }
 }
--- src/main/java/dev/br/pauloroberto/auth/domain/user/CommonUser.java
+++ src/main/java/dev/br/pauloroberto/auth/domain/user/CommonUser.java
@@ -3,6 +3,7 @@ package dev.br.pauloroberto.auth.domain.user;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
@@ -13,5 +14,6 @@ public class CommonUser extends User {
 
     public CommonUser(Long id, String username, String password) {
             super(id, username, password, "USER");
+            new SimpleGrantedAuthority("ROLE_USER");
         }
 }
