04bd04e0312785f16d0e303f8b762a358189a6b8
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-22 16:01:48 -0400

    Updated

--- src/main/java/dev/br/pauloroberto/auth/ApiUserAuthenticationApplication.java
+++ src/main/java/dev/br/pauloroberto/auth/ApiUserAuthenticationApplication.java
@@ -1,7 +1,10 @@
 package dev.br.pauloroberto.auth;
 
+import dev.br.pauloroberto.auth.domain.user.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiUserAuthenticationApplication {
@@ -10,4 +13,13 @@ public class ApiUserAuthenticationApplication {
         SpringApplication.run(ApiUserAuthenticationApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(UserRepository users) {
+        return args -> {
+//            users.save(new CommonUser(null, "user", "user"));
+//            users.save(new AdminUser(null, "admin", "admin"));
+//            System.out.println("user password INPUT: " + users.findByUsername("user").get().getPassword());
+//            System.out.println(new BCryptPasswordEncoder().encode("user"));
+        };
+    }
 }
